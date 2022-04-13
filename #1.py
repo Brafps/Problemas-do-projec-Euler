e605d7545435a38acb0499653fa436df4ca7d292
Breno Sampaio <br.sampaio@outlook.com>
2022-04-12 23:42:26 -0300

    Solução 1

---
+++ #1.py
@@ -0,0 +1,18 @@
+'''
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+'''
+
+''''
+Note que para a solução desse problemas, devemos lembrar que, se fizermos a soma simples, estaremos contando o múltiplos de 15 duas vezes. Logo, temos que pensar em uma maneira de resolver esse problema.
+'''
+
+
+
+soma = 0                       # Definindo a variável de acumulação
+for i in range(1000):
+    if (i%3 == 0 or i%5 == 0): # Aqui o "ou" é bem vindo, pois faz o número ser contado apenas uma vez
+        soma = soma+i          # Como a soma vai acumulando sobre ela mesma, exitia a necessidade de defini-la ante da função for
+
+print(soma)
