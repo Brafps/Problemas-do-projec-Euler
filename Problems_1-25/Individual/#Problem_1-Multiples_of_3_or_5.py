c5aee6b278ae9e19e9dafa64cae936889ec46936
Breno Sampaio <br.sampaio@outlook.com>
2022-05-25 12:43:37 -0300

    P1

--- Problems_1-25/Individual/#Problem_1-Multiples_of_3_or_5.py
+++ Problems_1-25/Individual/#Problem_1-Multiples_of_3_or_5.py
@@ -8,11 +8,8 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 Note que para a solução desse problemas, devemos lembrar que, se fizermos a soma simples, estaremos contando o múltiplos de 15 duas vezes. Logo, temos que pensar em uma maneira de resolver esse problema.
 '''
 
-
-
-soma = 0                       # Definindo a variável de acumulação
+soma = 0                       
 for i in range(1000):
-    if (i%3 == 0 or i%5 == 0): # Aqui o "ou" é bem vindo, pois faz o número ser contado apenas uma vez
-        soma = soma+i          # Como a soma vai acumulando sobre ela mesma, exitia a necessidade de defini-la ante da função for
-
+    if (i%3 == 0 or i%5 == 0): 
+        soma = soma+i          
 print(soma)
